7cdaac8e467ed1c8b09f43fdbd6e41df1f17d1be
unknown <anishpatil2909@gmail.com>
2026-07-10 21:17:49 +0530

    Cleaned dataset, tokenised

--- EDA.py
+++ EDA.py
@@ -19,18 +19,20 @@ def calc_avg(list_length):
     return total / len(df)
 
 # print the dataset 
-print(df.head())
+def print_head():
+    return df.head()
 
 # check the null values
-print(df.isnull().sum())
+def check_null():
+    return df.isnull().sum()
 
-print(df.columns)
+def columns_name():
+    return df.columns
 
 # distribution of sentiments
-print(df['sentiment'].value_counts())
+def distribution_sentiment():
+    return df['sentiment'].value_counts()
 
-# shape
-print(df.shape)
 
 # print(df['review'][0])
 
@@ -39,12 +41,15 @@ for i in range(len(df)):
     length = check_length(df['review'][i])
     list_length.append(length)
 
-print(f"The max len is : {max(list_length)}")
-print(f"The max len is : {min(list_length)}")
-print(f"The max len is : {calc_avg(list_length)}")
+def lengh_analysis():
+    print(f"The max len is : {max(list_length)}")
+    print(f"The max len is : {min(list_length)}")
+    print(f"The max len is : {calc_avg(list_length)}")
 
-plt.figure(figsize=(12, 6))
-plt.hist(list_length, bins = 50)
-plt.xlabel("Number of Words")
-plt.ylabel("Number of Reviews")
-# plt.show()
+def graph_wordDistribution():
+    plt.figure(figsize=(12, 6))
+    plt.hist(list_length, bins = 50)
+    plt.xlabel("Number of Words")
+    plt.ylabel("Number of Reviews")
+    plt.show()
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,51 @@
+import EDA
+import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from keras_preprocessing.text import Tokenizer
 
+# cleaning 
+df = EDA.df
+
+def clean_review(review):
+    final_review = ""
+    inside = False
+
+    for ch in review:
+        if ch == "<":
+            inside = True
+        elif ch == ">":
+            inside = False
+        elif inside == False:
+            final_review += ch
+
+    return final_review
+
+def cleaning_dataset():
+    df['clean'] = ''
+    for i in range(len(df)):
+        review = df['review'][i]
+        final_review = clean_review(review)
+        df['clean'][i] = final_review
+    return df
+
+df = cleaning_dataset()
+print(df['clean'][0])
+print(df['review'][0])
+
+
+def tokenizing():
+    df['np_sequences'] = None
+    
+    tokenizer = Tokenizer(num_words= 150000, oov_token="OOV")
+    tokenizer.fit_on_texts(df['clean'])
+    for i in range(len(df)):
+        review = df['clean'][i]
+        sequences = tokenizer.texts_to_sequences([review])
+        df.at[i, 'np_sequences'] = np.array(sequences)
+    return df
+
+df = tokenizing()
+print(df['clean'][0])
+print(df['review'][0])
+print(df['np_sequences'][0])
